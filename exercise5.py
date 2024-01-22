93351165c2eacdd628c4c894f1f9495df72a88f9
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 14:55:44 +0800

    Done with psuedocode

---
+++ exercise5.py
@@ -0,0 +1,7 @@
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+# define a function
+# create 2 variables one for the first item, second is the last item
+# if fist item and second item is same, funtion returns true
+# otherwise, function returns false
